ef3495fa41d834cc2bcc0cfdbdf8eb786211ba80
Apollo Truong <atruo017@ucr.edu>
2018-11-05 02:56:34 -0800

    partly changing node

--- ai.py
+++ ai.py
@@ -1,5 +1,9 @@
 import numpy as np
 import math
+import collections
+import itertools
+
+##############################################################
 
 w, h = 3, 3;
 
@@ -14,65 +18,83 @@ puzzle[2] = [7,8,6]
 defaultPosRow = [2,0,0,0,1,1,1,2,2,2]
 defaultPosCol = [2,0,1,2,0,1,2,0,1,2]
 
-def findStar(M):
-	for i in range(w):
-		for j in range(h):
-			if M[i][j] == 0:
-				return [i,j]
-def moveStarUp(M):
-	pos = findStar(M)
-	row = pos[0]
-	col = pos[1]
-	if pos[0] == 0:
-		#print ("Cannnot move Up")
-		return 0
-	else:
-		M[row][col], M[row - 1][col] = M[row - 1][col], M[row][col]
-		return 1
-def moveStarDown(M):
-	pos = findStar(M)
-	row = pos[0]
-	col = pos[1]
-	if pos[0] == 2:
-		#print ("Cannnot move Down")
-		return 0
-	else:
-		M[row][col], M[row + 1][col] = M[row + 1][col], M[row][col]
-		return 1
-def moveStarLeft(M):
-	pos = findStar(M)
-	row = pos[0]
-	col = pos[1]
-	if pos[1] == 0:
-		#print ("Cannnot move Left")
-		return 0
-	else:
-		M[row][col], M[row][col - 1] = M[row][col - 1], M[row][col]
-		return 1
-def moveStarRight(M):
-	pos = findStar(M)
-	row = pos[0]
-	col = pos[1]
-	if pos[1] == 2:
-		#print ("Cannnot move Right")
-		return 0
-	else:
-		M[row][col], M[row][col + 1] = M[row][col + 1], M[row][col]
-		return 1
+###############################################################
+
+# Manhattan Distance !!
+# Takes the distance of parameters 1RC , 2RC respectively
+def distance(t0,t1,s0,s1):
+	return math.fabs(t0 - s0) + math.fabs(t1 - s1)
+
+# Check the distance from x to its supposed position 1-8
+def distanceCheck(M,x):
+	d = distance(findRow(M,x),findCol(M,x),defaultPosRow[x],defaultPosCol[x])
+	
+	# debug
+	#print("defaultPosRow and defaultPosCol")
+	#print(defaultPosRow[x],defaultPosCol[x])
+	return d
+
+def totalDistanceCheck(M):
+	sum = 0
+	for i in range(8):
+		sum = sum + distanceCheck(M,i)
+	return sum
+
+
+class Puzzle:
+	def __init__(self,M):
+		self.board = M
+
+	def compareSoln(self):
+		soln = [[0 for x in range(w)] for y in range(h)] 
+		soln[0] = [1,2,3]
+		soln[1] = [4,5,6]
+		soln[2] = [7,8,0]
+
+		if self.M == soln:
+			return 1
+		else:
+			return 0
+
+	def validMoves(self):
+
+		# where M is a matrix
+		def findStar(M):
+			for i in range(w):
+				for j in range(h):
+					if M[i][j] == 0:
+						return [i,j]
+		moves = []
+		r, c =findStar(self.board)
+		if r < 2:
+			moves.append('D')
+		if r > 0:
+			moves.append('U')
+		if c < 2:
+			moves.append('R')
+		if c > 0:
+			moves.append('L')
+		return moves 
+
+	def copy(self):
+		board = self.board
+		return board
+
+
+
+	def move(self,fro,to):
+		copy = self.copy()
+		dodo = self.validMoves()
+		i,j = fro 
+		r,c = to
+		copy.board[i][j], copy.board[r][c] = copy.board[r][c], copy.board[i][j]
+		return copy
+
+
 def puzzlePrint(M):
 	print(np.matrix(M))
 	print()
-	return
-def compareSoln(M):
-	soln = [[0 for x in range(w)] for y in range(h)] 
-	soln[0] = [1,2,3]
-	soln[1] = [4,5,6]
-	soln[2] = [7,8,0]
 
-	if soln == M:
-		return 1
-	else:
-		return 0
 # Find the row of the x
 def findRow(M,x):
 	for i in range(w):
@@ -86,29 +108,14 @@ def findCol(M,x):
 			if M[i][j] == x:
 				return j
 
-# Manhattan Distance !!
-# Takes the distance of parameters 1:RC , 2:RC respectively
-def distance(t0,t1,s0,s1):
-	return math.fabs(t0 - s0) + math.fabs(t1 - s1)
 
-# Check the distance from x to its supposed position 1-8
-def distanceCheck(M,x):
-	d = distance(findRow(M,x),findCol(M,x),defaultPosRow[x],defaultPosCol[x])
-	
-	# debug
-	#print("defaultPosRow and defaultPosCol")
-	#print(defaultPosRow[x],defaultPosCol[x])
-	return d
 
 # Check the distance of the arrangement from the solution (goal)
 # Smallest number before goal is 2
 # Since there are 2 movements
-def totalDistanceCheck(M):
-	sum = 0
-	for i in range(8):
-		sum = sum + distanceCheck(M,i)
-	return sum
 
+
+# uniform cost search
 def uniformCS(M):
 	""" FROM HAND-OUT
 	function general-search( problen, QUEUEING-FUNCTION)
@@ -135,7 +142,7 @@ class Queue:
 		return self.items == []
 
 	def enqueue(self, item):
-		self.iterms.insert(0,item)
+		self.items.insert(0,item)
 
 	def dequeue(self):
 		return self.items.pop()
@@ -143,24 +150,56 @@ class Queue:
 	def size(self):
 		return len(self.items)
 
-q = Queue()
+class Node:
+	def __init__(self,state=None,parent=None,cost=0,depth=0,children=[]):
+		self.state = state 
+		self.parent = parent
+		self.cost = cost
+		self.depth = depth
+		self.children = children
 
-q.enqueue(4)
+	def is_goal(self,goal_state):
+		return is_goal_state(self.state, goal_state)
+
+	def expand(self):
+		new_states = operator(self.state)
+		self.children = []
+		for state in new_states:
+			self.children.append(Node(state,self,self.cost + 1,self.depth + 1))
 
 
+	def parents(self):
+		current_node = self
+		while current_node.parent:
+			yield current_node.parent:
+			current_node = current_node.parent
+
+	def gn(self):
+		costs = self.cost
+		for parent in self.parents():
+			costs += parent.cost
+
+		return costs
+
+def checksol
+
+
+# main stuff
 
 print ("Welcome to Apollo Truong's 8-puzzle solver.")
 userInput = input("Type \'1\' to use a default puzzle, or \'2\' to enter your own puzzle. ") 
 
-puzzlePrint(puzzle)
+board = [[1,2,3],[4,5,0],[6,7,8]]
+puzzle = Puzzle(board)
 
-print("Distance is")
+s = AStar(puzzle)
+p = s.solve()
 
-d = distanceCheck(puzzle,0)
-print(d)
+steps = 0
 
-#return 1 if true, 0 false
-print(compareSoln(puzzle))
+for node in p:
+	print(node.moves)
+	node.puzzlePrint(puzzle)
+	steps = steps + 1
 
-print("Total distance is: ",totalDistanceCheck(puzzle))
-
+print("Total number of steps: " + str(steps))
